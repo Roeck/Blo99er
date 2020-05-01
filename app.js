751d80fc1cd1488f99ae85d55d2bfe1ff902ead6
Roeck <rons9n@gmail.com>
2020-05-01 19:31:35 -0400

    post functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const _ = require('lodash');
 
 const homeStartingContent = 'This is a blog post.';
 const aboutContent = 'My info.';
@@ -15,8 +16,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+let posts = [];
+
 app.get('/', function(req, res) {
-  res.render('home', { newText: homeStartingContent });
+  res.render('home', { newText: homeStartingContent, posts: posts });
 });
 
 app.get('/about', function(req, res) {
@@ -25,6 +28,35 @@ app.get('/about', function(req, res) {
 
 app.get('/contact', function(req, res) {
   res.render('contact', { contact: contactContent });
+});
+
+app.get('/compose', function(req, res) {
+  res.render('compose');
+});
+
+app.post('/compose', function(req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+
+  res.redirect('/');
+});
+
+app.get('/posts/:postName', function(req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render('post', {
+        title: post.title,
+        content: post.content
+      });
+    }
+  });
 });
 
 app.listen(3000, function() {
