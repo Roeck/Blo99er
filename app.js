0f3201a0119d65affcffd0843704d90ce1950762
Roeck <rons9n@gmail.com>
2020-05-14 18:36:40 -0400

    ready for deploying

--- app.js
+++ app.js
@@ -71,5 +71,5 @@ app.get('/contact', function(req, res) {
 });
 
 app.listen(3000, function() {
-  console.log('Server started on port 3000');
+  console.log('Server started on port 3000', 'blo99er.herokuapp.com');
 });
